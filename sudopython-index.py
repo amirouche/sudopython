f04d91187024503a6395cd02eacb10963cf2af52
Amirouche <amirouche@hyper.dev>
2021-02-20 12:53:37 +0100

    varia
    - simplify regex because strings are lower case,
    
    - remove commented code,
    
    - use summary and description,
    
    - only store documents that have at least one stem.

--- sudopython-index.py
+++ sudopython-index.py
@@ -24,24 +24,17 @@ html2text = handler.handle
 pypi = LSM('pypi.okvslite')
 db = LSM('db.okvslite')
 
-RE_WORD = re.compile(r"[A-Za-z]+")
+RE_WORD = re.compile(r"[a-z]+")
 RE_TOKENS = re.compile(r"[a-z0-9._-]+")
 
 stemmer = Stemmer.Stemmer('english')
 stemit = stemmer.stemWords
 
-
-# def init_counter(db, key):
-#     db[lexode.pack((key,))] = lexode.pack(())
-
-
-# init_counter(db, SUBSPACE_STEM_DOCUMENT_COUNTER)
-
 counter = Counter()
 
 for index, (key, value) in enumerate(pypi):
     name = lexode.unpack(key)[0]
-    value = lexode.unpack(value)[0]
+    summary, description = lexode.unpack(value)
     if index % 1_000 == 0:
         print(index, name, file=sys.stderr)
 
@@ -49,32 +42,31 @@ for index, (key, value) in enumerate(pypi):
     uid = ulid.new().bytes
 
     # Prepare document
-    document = name + " - " + value
+    document = name + " - " + summary + " - " + description
     document = unidecode(document.lower())
-    document = html2text(document)
-
-    # Prepare forward index and store preview
-    preview = ' '.join(document.split())[:1024]
-    db[lexode.pack((SUBSPACE_PREVIEW, uid))] = preview.encode('utf8')
 
     # Prepare words
     words = [unidecode(x) for x in RE_WORD.findall(document) if 3 <= len(x) <= 255]
-    # Store document with term frequency
-    counter_words = Counter(words)
-    counter_words = tuple(counter_words.items())
-    db[lexode.pack((SUBSPACE_FOWARD, uid))] = lexode.pack((document, counter_words))
 
     # Yes, stemit will produce multiple times the same stem.
     stems = set(stemit(words))
 
+    if not stems:
+        continue
+
+    # forward index
+    counter_words = Counter(words)
+    counter_words = tuple(counter_words.items())
+    db[lexode.pack((SUBSPACE_FOWARD, uid))] = lexode.pack((document, counter_words))
     # Store stems with backward index
     for stem in stems:
         db[lexode.pack((SUBSPACE_BACKWARD, stem, uid))] = b''
 
+    # store preview
+    preview = ' '.join(document.split())[:1024]
+    db[lexode.pack((SUBSPACE_PREVIEW, uid))] = preview.encode('utf8')
+
     # update stem counter
-    # counter = lexode.unpack(db[lexode.pack((SUBSPACE_STEM_DOCUMENT_COUNTER,))])
-    # counter = dict(counter)
-    # counter = Counter(counter)
     counter += Counter(stems)
 
     # Store "tokens" with bbkh
